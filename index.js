8bbfe89b1aef9ed7ca3d4fdaed89fe2141dceaff
Patrick Jeremiah <patrickjerry59@yahoo.com>
2022-06-02 00:52:58 +0000

    fix: the loop through to play again.

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 //Guess game generator.
 
 // const userName = parseFloat(prompt('Enter name: '))
-const userNumb = parseFloat(prompt("Enter number:"))
+// const userNumb = parseFloat(prompt("Enter number:"))
+
+// const userName = prompt("Enter your name: ")
+
 
 //Function for generating randomly from range 1 to 2
 const rangeGenerator = (start=2) => {
   let rand = Math.random() * start
   let randomWhole = Math.ceil(rand)
-
   return randomWhole
 }
 
-const guessRW = (computerguess, userNumb) => {
+
+const guessRW = (computerguess, winning = "yes") => {
   
-  for (let incre = 1; incre < userNumb; incre++){
-    
+   while (winning === "yes"){
+    const userNumb = parseFloat(prompt("Enter number:"))
+     
     if (userNumb <= 2 && userNumb === computerguess){
       console.log(`You guess right ${computerguess} `)
-      guessRW(computerguess, userNumb)
+      winning = prompt('Do you want to play again? ') 
       
     }
-   return console.log( `You guessed wrong ${ computerguess}`) 
+     else{
+       console.log( `You guessed wrong ${ computerguess}`)
+       break
+     }
   }
   
 }
 
-guessRW(rangeGenerator(), userNumb)
+guessRW(rangeGenerator())
 
 
 
