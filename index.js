9e25d83332fde6c2fb955ffac531112628726f77
Patrick Jeremiah <patrickjerry59@yahoo.com>
2022-06-05 00:49:08 +0000

    Fixed the range boundary

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 //Guess game generator.
 
-// const userName = parseFloat(prompt('Enter name: '))
-// const userNumb = parseFloat(prompt("Enter number:"))
 
 // const userName = prompt("Enter your name: ")
 
 
 //Function for generating randomly from range 1 to 2
 const rangeGenerator = (start=2) => {
-  let rand = Math.random() * start
-  let randomWhole = Math.ceil(rand)
+  let rand = Math.random() * start;
+  let randomWhole = Math.ceil(rand);
   return randomWhole
 }
 
-
-const guessRW = (computerguess, winning = "yes") => {
-  
-   while (winning === "yes"){
-    const userNumb = parseFloat(prompt("Enter number:"))
-     
-    if (userNumb <= 2 && userNumb === computerguess){
-      console.log(`You guess right ${computerguess} `)
-      winning = prompt('Do you want to play again? ') 
+// Function for the guess game.
+const guessRW = (computerGuesser, winning = "yes", range=2) => {
+  try{
+    while (winning === "yes") {
       
-    }
-     else{
-       console.log( `You guessed wrong ${ computerguess}`)
-       break
-     }
-  }
+      const userNumber = parseFloat(prompt("Enter number within the starting range: "));
   
+      if (userNumber > computerGuesser && userNumber > range){
+        return console.log(`${userNumber} out of range ${computerGuesser}`)
+      }
+      else if (isNaN(userNumber)){
+        return console.log(`${userNumber}, it's a string`)
+      }  
+      else{
+        if (userNumber === computerGuesser){
+          console.log(`${computerGuesser}, you won! `)
+          winning = prompt('Do you want to play again? ')
+        }else{
+          console.log( `${ computerGuesser}, your guess is wrong!`)
+          {break}
+        }        
+      }
+    }
+  }catch(error){
+    return console.log("Something is wrong somewhere!" | error.message)
+  }
 }
 
 guessRW(rangeGenerator())
-
+// rangeGenerator(5)
 
 
 
